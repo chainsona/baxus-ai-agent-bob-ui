56b02e34629301b8248bdc9d9fdead9d5ade525d
Soona <code@chainsona.dev>
2025-04-28 20:35:41 +0200

    feat: health check

---
+++ src/app/api/health/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  return NextResponse.json(
+    {
+      status: 'healthy',
+      timestamp: new Date().toISOString(),
+    },
+    { status: 200 }
+  );
+};
