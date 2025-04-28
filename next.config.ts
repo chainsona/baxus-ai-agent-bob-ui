46eec37159f46569325c08e300489c4d15346b47
Soona <code@chainsona.dev>
2025-04-28 21:59:19 +0200

    chore: allow images from all sources

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   images: {
-    domains: ['d1w35me0y6a2bb.cloudfront.net', 'assets.baxus.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
   },
 };
 
